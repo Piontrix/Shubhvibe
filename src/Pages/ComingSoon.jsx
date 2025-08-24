67355f9cc0733350f21e4f0e9be9c1463d29476e
Aakash Gaur <aakash.sunrise23@gmail.com>
2025-08-24 23:33:42 +0530

    Updated mobile number

--- src/Pages/ComingSoon.jsx
+++ src/Pages/ComingSoon.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../assets/ShubhVibeLogo.png";
 
 const ComingSoon = () => {
+  const phoneNumber = import.meta.env.VITE_PHONE_NUMBER;
+
   return (
     <div className="min-h-screen bg-white text-gray-900 flex flex-col items-center justify-center text-center px-6">
       <img src={logo} alt="ShubhVibe Events" className="w-56 mb-6 drop-shadow-lg" />
@@ -12,13 +14,13 @@ const ComingSoon = () => {
       {/* Buttons */}
       <div className="mt-8 flex gap-6">
         <a
-          href="tel:+919876543210"
+          href={`tel:+${phoneNumber}`}
           className="px-5 py-2 rounded-lg border border-[#d4af37] text-[#d4af37] hover:bg-[#d4af37] hover:text-black transition"
         >
           📞 Call Us
         </a>
         <a
-          href="https://wa.me/919876543210"
+          href={`https://wa.me/+${phoneNumber}`}
           className="px-5 py-2 rounded-lg border border-[#d4af37] text-[#d4af37] hover:bg-[#d4af37] hover:text-black transition"
           target="_blank"
           rel="noopener noreferrer"
